6a8ccb53db5e41882af0c84ce30c06c6285ba8ec
= <=>
2023-01-15 19:38:01 -0300

    primeiro commit projeto

---
+++ src/main/java/br/com/devsuperior/umlspring/UmlSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.umlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlSpringApplication.class, args);
+	}
+
+}
