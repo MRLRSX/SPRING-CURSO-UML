f52c173adb7df5681399735770074c608c97bc66
= <=>
2023-01-17 20:03:06 -0300

    implements

--- src/main/java/br/com/devsuperior/umlspring/repositories/EstadoRepository.java
+++ src/main/java/br/com/devsuperior/umlspring/repositories/EstadoRepository.java
@@ -1,4 +1,9 @@
 package br.com.devsuperior.umlspring.repositories;
 
-public interface EstadoRepository {
+import br.com.devsuperior.umlspring.entities.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
 }
