d1fd9e7e6f3765ae96da9878a0b1d12d9287f94b
= <=>
2023-01-17 04:18:14 -0300

    corrigir erros

--- src/main/java/br/com/devsuperior/umlspring/entities/Categoria.java
+++ src/main/java/br/com/devsuperior/umlspring/entities/Categoria.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name="tb_name")
+@Table(name="tb_categoria")
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1l;
---
+++ src/main/resources/import.sql
@@ -0,0 +1,28 @@
+
+insert into tb_categoria(name)values('informatica');
+insert into tb_categoria(name)values('livros');
+insert into tb_categoria(name)values('cosmetico');
+
+insert into tb_produto(nome, preco)values('MacBook Pro M1', 12000.00);
+insert into tb_produto(nome, preco)values('MacBook Pro M2', 8000.00);
+insert into tb_produto(nome, preco)values('MacBook Pro M1 16', 27000.00);
+insert into tb_produto(nome, preco)values('IPHONE 14 PRO MAX', 12700.00);
+insert into tb_produto(nome, preco)values('MacBook 14 PRO', 8500.00);
+insert into tb_produto(nome, preco)values('MacBook 13 PRO MAX', 8600.00);
+insert into tb_produto(nome, preco)values('MacBook 13 PRO', 7000.00);
+insert into tb_produto(nome, preco)values('MacBook 12 PRO MAX', 6500.00);
+insert into tb_produto(nome, preco)values('Segredos da Mente Milhonaria', 30.00);
+insert into tb_produto(nome, preco)values('Pai Rico e Pai Pobre', 40.00);
+insert into tb_produto(nome, preco)values('Shampo Clear Men', 150.00);
+
+insert into tb_produto_categoria(produto_id, categoria_id)values(1, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(2, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(3, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(4, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(5, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(6, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(7, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(8, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(9, 1);
+insert into tb_produto_categoria(produto_id, categoria_id)values(10, 2);
+insert into tb_produto_categoria(produto_id, categoria_id)values(11, 3);
